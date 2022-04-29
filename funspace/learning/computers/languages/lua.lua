82b8f78a6501fe37e1872b059e3fca33dda6a628
EugeneFelix <ontharros@gmail.com>
2022-04-29 20:27:18 +0530

    commit

---
+++ funspace/learning/computers/languages/lua.lua
@@ -0,0 +1,14 @@
+--[[ Learning Lua, one of my favorita languages! --]]
+--
+print("Hello, Roberto Ierusalimschy  from 1993\n")
+function max(num1, num2)
+    if (num1 > num2) then
+      result = num1;
+   else
+      result = num2;
+   end
+   return result; 
+end
+
+var = max(10,20)
+print(var)
