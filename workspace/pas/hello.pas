b181bc3b6ff488f129aac06e41e1f58660c4207f
EugeneFelix <ontharros@gmail.com>
2022-08-03 22:38:18 +0530

    commit

---
+++ workspace/pas/hello.pas
@@ -0,0 +1,19 @@
+program hello;
+uses crt;
+var a, b, c, s, i, n, res: Integer;
+const PI = 3.14;
+begin
+  (*This is a comment*)
+  writeln('Hello There...');
+  readln(a, b, c);
+  s := a + b + c;
+  res := 0;
+  n := 5;
+  writeln(a div b);
+  writeln(s + PI);
+  for i  := 1 to n do
+    begin
+    res := res + 5;
+  writeln(res);
+  end
+end.
